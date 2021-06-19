976c1b8ffdc421289006e245a9e01c639bb95ea6
Deivid Gustavo Mattos <deivid@dgmattos.com.br>
2021-06-19 18:58:48 -0300

    Adicionado arquivo de cores

---
+++ lib/Theme/Cores.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class Cores{
+
+  static Color laranja = Colors.orange[900];
+  static Color cinza_fundo = Colors.grey[900];
+}
