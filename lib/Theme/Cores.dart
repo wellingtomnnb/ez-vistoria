895f970f95d9375f41cd57eef7e06467d9a5cd12
Deivid Gustavo Mattos <deivid@dgmattos.com.br>
2021-06-19 21:30:56 -0300

    Atualizacao das cores

--- lib/Theme/Cores.dart
+++ lib/Theme/Cores.dart
@@ -5,4 +5,6 @@ class Cores{
 
   static Color laranja = Colors.orange[900];
   static Color cinza_fundo = Colors.grey[900];
+  static Color texto_branco = Colors.white;
+  static Color cinza_claro = Colors.grey;
 }
