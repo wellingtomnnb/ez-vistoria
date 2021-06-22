30f6f018727ccd77ad0b5acee4390725ab1757d0
Wellington Braga <sabiomael@gmail.com>
2021-06-22 18:43:44 -0300

    Atualização de Menus Laterais

--- lib/Pages/CriarPage.dart
+++ lib/Pages/CriarPage.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'HomePage.dart';
 import 'ListItensPage.dart';
+import 'LoginPage.dart';
+import 'SobrePage.dart';
+import 'VistoriasPage.dart';
 
 class CriarPage extends StatefulWidget {
   @override
@@ -13,6 +17,41 @@ class _CriarPageState extends State<CriarPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Nova Vistoria')),
+        drawer: Drawer(
+          child: ListView(children: <Widget> [
+            DrawerHeader(
+              child: Text("Menu"),
+              decoration: BoxDecoration(
+                  color: Colors.grey[350],
+                  image: DecorationImage(
+                      image: NetworkImage('https://s01.jetimgs.com/trs2SR8vLcluowtNjIWNZFqybLrndFutqc9yZj_xlCtR2hBJIrlJD9ML4drZC6Q5WuP5B6jPb9iy910siVufFOwkPVwAdvxz_Zx0dS8NQ0PqeUfNc3QF/documentos.png')
+                  )
+              ),
+            ),
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () =>Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()))
+            ),
+            ListTile(
+                leading: Icon(Icons.check_circle_outline),
+                title: Text("Vistorias"),
+                onTap: () =>Navigator.of(context).push(MaterialPageRoute(builder: (context) => VistoriasPage()))
+            ),
+            ListTile(
+                leading: Icon(Icons.info_outline),
+                title: Text("Sobre"),
+                onTap: () =>Navigator.of(context).push(MaterialPageRoute(builder: (context) => SobrePage()))
+            ),
+            ListTile(
+                leading: Icon(Icons.power_settings_new),
+                title: Text("Sair"),
+                onTap: () =>Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()))
+            )
+          ],
+          ),
+        ),
+
         body: Center(
             child: Column(
                 children: [
